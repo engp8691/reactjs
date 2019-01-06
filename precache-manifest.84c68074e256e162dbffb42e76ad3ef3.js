bc8daafd8335654ab99fca7267a6a995e5526577
root <root@rtc.netbriefings.com>
2019-01-05 21:00:03 -0500

    Second commit

---
+++ precache-manifest.84c68074e256e162dbffb42e76ad3ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e5c756d9e8babc7374",
+    "url": "/reactjs/static/js/runtime~main.a4e5c756.js"
+  },
+  {
+    "revision": "3f2e04ff0d51d29c58b1",
+    "url": "/reactjs/static/js/main.3f2e04ff.chunk.js"
+  },
+  {
+    "revision": "2d7277299183cfeb1b91",
+    "url": "/reactjs/static/js/1.2d727729.chunk.js"
+  },
+  {
+    "revision": "3f2e04ff0d51d29c58b1",
+    "url": "/reactjs/static/css/main.9bb8245e.chunk.css"
+  },
+  {
+    "revision": "4aa958b71433b596e1a8d3dee1050e82",
+    "url": "/reactjs/index.html"
+  }
+];
